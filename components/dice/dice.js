25652ac77d93d98e8e3efc3e8038238b77ccf95c
Nicolás M. Pardo <nicolasm@nikodermus.media>
2019-02-27 12:07:21 -0500

    fix(dice.js): Smooth rolling

--- components/dice/dice.js
+++ components/dice/dice.js
@@ -2,43 +2,67 @@ const dice_sides = ["front", "right", "back", "top", "bottom", "left"];
 const roll_history = { last: null };
 
 const disableClick = htmlElement => {
+  console.log("disabling");
   htmlElement.classList.add("disable-click");
 };
 
 const enableClick = htmlElement => {
+  console.log("enabling");
   htmlElement.classList.remove("disable-click");
 };
 
 const getWrap = htmlElement => htmlElement.querySelector(".cube__wrap");
 
-const getDiceSide = (sides = []) =>
-  sides[Math.floor(Math.random() * sides.length)];
+const getDiceSide = (sides = []) => {
+  const random = Math.floor(Math.random() * sides.length);
+  return [random, sides[random]];
+};
+
+const releaseDice = dice => {
+  enableClick(dice);
+  dice.removeEventListener("transitionend", releaseDice);
+};
 
 const rollTheDice = ({ target }) => {
+  let timeout = 0;
+
   const dice = target.closest(".cube");
   const dice_wrap = getWrap(dice);
   const last_roll = roll_history.last ? roll_history[roll_history.last] : "";
 
-  dice_wrap.classList.remove(`cube__wrap--${last_roll}`);
   disableClick(dice);
-  dice_wrap.style.animationPlayState = "running";
-  const roll = getDiceSide(dice_sides);
+  const [index, roll] = getDiceSide(dice_sides);
 
-  setTimeout(() => {
+  if (last_roll == roll) {
+    timeout = 1000;
+    dice_wrap.classList.remove(`cube__wrap--${last_roll}`);
+    dice_wrap.classList.add(`cube__wrap--fake-roll-${index}`);
+  }
+
+  const wait_for_roll = setTimeout(() => {
+    dice.addEventListener("transitionend", () => releaseDice(dice));
+
+    dice_wrap.classList.remove(
+      `cube__wrap--${last_roll}`,
+      `cube__wrap--fake-roll-${index}`
+    );
     dice_wrap.classList.add(`cube__wrap--${roll}`);
-    enableClick(dice);
 
     const date = new Date();
     roll_history[date.valueOf()] = roll;
     roll_history.last = date.valueOf();
-  }, 2000);
+
+    clearTimeout(wait_for_roll);
+  }, timeout);
 };
 
 const cube = document.querySelector(".cube");
 cube.addEventListener("click", rollTheDice);
 
-// Object.entries(roll_history).forEach(([key, value]) => {
-//   if (Number(key)) {
-//     console.log(new Date(Number(key)).toDateString(), value);
-//   }
-// });
+const getRollHistory = () => {
+  Object.entries(roll_history).forEach(([key, value]) => {
+    if (Number(key)) {
+      console.log(new Date(Number(key)).toDateString(), value);
+    }
+  });
+};
